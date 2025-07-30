25682e9c97066858c3ebdd84de9700f93aaeea55
unknown <sasha.lartsev@gmail.com>
2025-07-31 03:18:40 +0400

    Add install services for forecast

--- Assets/Installers/WeatherForecastInstaller.cs
+++ Assets/Installers/WeatherForecastInstaller.cs
@@ -1,11 +1,25 @@
+using Models;
+using SO;
+using UnityEngine;
+using Views;
 using Zenject;
 
 namespace Installers
 {
     public class WeatherForecastInstaller : MonoInstaller
     {
+        [SerializeField] private WeatherSettingsSO _settings;
+        [SerializeField] private WeatherView _view;
         public override void InstallBindings()
         {
+            Container.BindInstance(_settings).AsSingle();
+            Container.BindInstance(_view).AsSingle();
+
+            Container.Bind<WeatherModel>().AsSingle();
+
+            Container.BindInterfacesAndSelfTo<WeatherPresenter>()
+                     .AsSingle()
+                     .NonLazy();
         }
     }
 }
